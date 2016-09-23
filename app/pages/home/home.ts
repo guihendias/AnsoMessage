f3bf01b9400a7129065c20b5cb7e25dc7dec1e91
Guilherme Dias <guilhermehenriquedias@hotmail.com>
2016-09-22 22:01:00 -0300

    Aula 09

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,19 +1,51 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Messages} from '../../util/messages';
+import { Geolocation } from 'ionic-native';
+
+declare var geolib: any;
 
 @Component({
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
   messagesList: any = [];
-  constructor(public navCtrl: NavController, private messages : Messages) {
+  constructor(public navCtrl: NavController, private messages: Messages) {
     this.initPage();
   }
 
-  initPage(){
+  private initPage() {
     this.messages.get(message => {
       this.messagesList.push(message);
     })
+
+    setInterval(() => {
+      this.getAllDistances();
+    }, 3000);
+  }
+
+  private getAllDistances() {
+    Geolocation.getCurrentPosition().then(resp => {
+      for (let i = 0; i < this.messagesList.length; i++) {
+        let message = this.messagesList[i];
+
+        message.distance = this.getDistance(
+          {
+            latitude: resp.coords.latitude,
+            longitude: resp.coords.longitude
+          },
+          {
+            latitude: message.lat,
+            longitude: message.lng
+          }
+        )
+      }
+    });
+  }
+
+  private getDistance(origin, destination) {
+    let distance = geolib.getDistacce(origin, destination);
+
+    return geolib.convertUnit('km',distance,2);
   }
 }
